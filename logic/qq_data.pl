0d41fc2be5d390db802071510aaca540bf6f5a9c
yago-san-fdz <yago.sanchez.fdz@gmail.com>
2024-06-02 18:10:47 +0200

    [fix] Cambios drásticos
    Existían varios problemas que no entendía dado el posicionamiento del error, resulta que estaba usando ñ y tildes y por eso saltaban errores
    
    Arreglé como detecta mis respuestas y el flujo de rasgos
    
    FALTA:
    Avisar al jugador de que piense en un personaje
    Cambiar y actualizar el readme y sus necesidades
    Evitar el tiene "hombre"

--- logic/qq_data.pl
+++ logic/qq_data.pl
@@ -1,26 +1,74 @@
-% Definición de cada personaje con sus rasgos
-pj(herman, [hombre, cabello_rojo, calvo, nariz_grande, ojos_marrones, cejas_gruesas]).
-pj(maria, [mujer, cabello_largo, sombrero, pendientes, cabello_castaño, ojos_marrones, boca_pequeña, cejas_delgadas, nariz_pequeña]).
-pj(claire, [mujer, gafas, sombrero, cabello_rojo, ojos_marrones, boca_pequeña, nariz_pequeña]).
-pj(charles, [hombre, bigote, rubio, ojos_marrones, labios_gruesos, boca_grande, orejas_grandes, patillas, nariz_pequeña]).
-pj(richard, [hombre, calvo, barba, ojos_marrones, orejas_grandes, bigote, rostro_largo, nariz_pequeña]).
-pj(eric, [hombre, rubio, gorra, sombrero, ojos_marrones, boca_grande, nariz_pequeña]).
-pj(alex, [hombre, bigote, cabello_negro, ojos_marrones, boca_grande, labios_gruesos, orejas_grandes, cabello_corto, nariz_pequeña]).
-pj(peter, [hombre, cabello_gris, cabello_blanco, nariz_grande, ojos_azules, cejas_gruesas, labios_gruesos, boca_grande, patillas]).
-pj(philip, [hombre, barba, cabello_negro, ojos_marrones, orejas_grandes, mejillas, mejillas_rosadas, cejas_delgadas, cabello_corto, nariz_pequeña]).
-pj(joe, [hombre, gafas, rubio, ojos_marrones, boca_pequeña, cabello_corto, nariz_pequeña]).
-pj(paul, [hombre, gafas, cabello_blanco, cabello_gris, ojos_marrones, boca_pequeña, orejas_grandes, cejas_gruesas, patillas, nariz_pequeña]).
-pj(david, [hombre, barba, rubio, ojos_marrones, orejas_grandes, patillas, nariz_pequeña]).
-pj(george, [hombre, rostro_triste, sombrero, cabello_blanco, cabello_gris, ojos_marrones, boca_grande, nariz_pequeña]).
-pj(frans, [hombre, cabello_corto, cejas_gruesas, cabello_rojo, ojos_marrones, boca_pequeña, nariz_pequeña]).
-pj(alfred, [hombre, bigote, barba, cabello_rojo, ojos_azules, boca_pequeña, orejas_grandes, cabello_largo, cabello_medio, nariz_pequeña]).
-pj(bernard, [hombre, cabello_castaño, sombrero, ojos_marrones, boca_pequeña, cejas_delgadas, nariz_grande]).
-pj(bill, [hombre, barba, cabello_rojo, ojos_marrones, orejas_grandes, mejillas, mejillas_rosadas, calvo, boca_pequeña, nariz_pequeña]).
-pj(anita, [mujer, cabello_largo, rubio, ojos_marrones, boca_pequeña, mejillas, mejillas_rosadas, cabello_medio, nariz_pequeña]).
-pj(robert, [hombre, rostro_triste, cabello_castaño, ojos_azules, orejas_grandes, nariz_grande, patillas, rostro_largo, mejillas, mejillas_rosadas]).
-pj(anne, [mujer, cabello_corto, pendientes, cabello_negro, ojos_marrones, boca_pequeña, nariz_grande]).
-pj(sam, [hombre, gafas, calvo, cabello_blanco, cabello_gris, ojos_marrones, boca_pequeña, nariz_pequeña]).
-pj(tom, [hombre, gafas, calvo, cabello_negro, ojos_azules, boca_pequeña, rostro_largo, nariz_pequeña]).
-pj(susan, [mujer, cabello_largo, cabello_blanco, cabello_gris, ojos_marrones, labios_gruesos, mejillas, mejillas_rosadas, nariz_pequeña, patillas]).
-pj(max, [hombre, bigote, cabello_negro, ojos_marrones, boca_grande, labios_gruesos, nariz_grande, orejas_grandes, cabello_corto]).
+% Definicion de personajes con nombre y caracteristicas
 
+personaje(nombre('herman'), caracteristicas([hombre, pelirrojo, calva, nariz_grande, ojos_marrones, cejas_gruesas])).
+personaje(nombre('maria'), caracteristicas([mujer, pelo_largo, sombrero, pendientes, pelo_castano, ojos_marrones, boca_pequena, cejas_finas, nariz_pequena])).
+personaje(nombre('claire'), caracteristicas([mujer, gafas, sombrero, pelirrojo, ojos_marrones, boca_pequena, nariz_pequena])).
+personaje(nombre('charles'), caracteristicas([hombre, bigote, pelo_rubio, ojos_marrones, labios_gruesos, boca_grande, orejas_grandes, raya_al_lado, nariz_pequena])).
+personaje(nombre('richard'), caracteristicas([hombre, calva, barba, ojos_marrones, orejas_grandes, bigote, cara_alargada, nariz_pequena])).
+personaje(nombre('eric'), caracteristicas([hombre, pelo_rubio, gorra, sombrero, ojos_marrones, boca_grande, nariz_pequena])).
+personaje(nombre('alex'), caracteristicas([hombre, bigote, pelo_negro, ojos_marrones, boca_grande, labios_gruesos, orejas_grandes, pelo_corto, nariz_pequena])).
+personaje(nombre('peter'), caracteristicas([hombre, canas, pelo_blanco, nariz_grande, ojos_azules, cejas_gruesas, labios_gruesos, boca_grande, raya_al_lado])).
+personaje(nombre('philip'), caracteristicas([hombre, barba, pelo_negro, ojos_marrones, orejas_grandes, mofletes, mejillas_sonrosadas, cejas_finas, pelo_corto, nariz_pequena])).
+personaje(nombre('joe'), caracteristicas([hombre, gafas, pelo_rubio, ojos_marrones, boca_pequena, pelo_corto, nariz_pequena])).
+personaje(nombre('paul'), caracteristicas([hombre, gafas, pelo_blanco, canas, ojos_marrones, boca_pequena, orejas_grandes, cejas_gruesas, raya_al_lado, nariz_pequena])).
+personaje(nombre('david'), caracteristicas([hombre, barba, pelo_rubio, ojos_marrones, orejas_grandes, raya_al_lado, nariz_pequena])).
+personaje(nombre('george'), caracteristicas([hombre, cara_triste, sombrero, pelo_blanco, canas, ojos_marrones, boca_grande, nariz_pequena])).
+personaje(nombre('frans'), caracteristicas([hombre, pelo_corto, cejas_gruesas, pelirrojo, ojos_marrones, boca_pequena, nariz_pequena])).
+personaje(nombre('alfred'), caracteristicas([hombre, bigote, barba, pelirrojo, ojos_azules, boca_pequena, orejas_grandes, pelo_largo, raya_al_medio, nariz_pequena])).
+personaje(nombre('bernard'), caracteristicas([hombre, pelo_castano, sombrero, ojos_marrones, boca_pequena, cejas_finas, nariz_grande])).
+personaje(nombre('bill'), caracteristicas([hombre, barba, pelirrojo, ojos_marrones, orejas_grandes, mofletes, mejillas_sonrosadas, calva, boca_pequena, nariz_pequena])).
+personaje(nombre('anita'), caracteristicas([mujer, pelo_largo, pelo_rubio, ojos_marrones, boca_pequena, mofletes, mejillas_sonrosadas, raya_al_medio, nariz_pequena])).
+personaje(nombre('robert'), caracteristicas([hombre, cara_triste, pelo_castano, ojos_azules, orejas_grandes, nariz_grande, raya_al_lado, cara_alargada, mofletes, mejillas_sonrosadas])).
+personaje(nombre('anne'), caracteristicas([mujer, pelo_corto, pendientes, pelo_negro, ojos_marrones, boca_pequena, nariz_grande])).
+personaje(nombre('sam'), caracteristicas([hombre, gafas, calva, pelo_blanco, canas, ojos_marrones, boca_pequena, nariz_pequena])).
+personaje(nombre('tom'), caracteristicas([hombre, gafas, calva, pelo_negro, ojos_azules, boca_pequena, cara_alargada, nariz_pequena])).
+personaje(nombre('susan'), caracteristicas([mujer, pelo_largo, pelo_blanco, canas, ojos_marrones, labios_gruesos, mofletes, mejillas_sonrosadas, nariz_pequena, raya_al_lado])).
+personaje(nombre('max'), caracteristicas([hombre, bigote, pelo_negro, ojos_marrones, boca_grande, labios_gruesos, nariz_grande, orejas_grandes, pelo_corto])).
+
+% Iniciar juego automaticamente al cargar el archivo
+:- initialization(iniciar_juego).
+
+% Predicado principal para jugar
+iniciar_juego :-
+    writeln('¿Quieres jugar? (s/n)'),
+    read_line_to_string(user_input, Respuesta),
+    (Respuesta == "s" -> jugar; writeln('¡Hasta luego!')).
+
+% Juego de preguntas
+jugar :-
+    findall(personaje(nombre(N), caracteristicas(Caracteristicas)), personaje(nombre(N), caracteristicas(Caracteristicas)), TodosPersonajes),
+    findall(Caracteristica, (personaje(_, caracteristicas(Caracteristicas)), member(Caracteristica, Caracteristicas)), TodosRasgos),
+    list_to_set(TodosRasgos, RasgosUnicos),
+    hacer_pregunta(TodosPersonajes, RasgosUnicos).
+
+hacer_pregunta([personaje(nombre(N), _)], _) :-
+    format('El personaje es: ~w~n', [N]).
+hacer_pregunta(Personajes, Rasgos) :-
+    seleccionar_mejor_rasgo(Personajes, Rasgos, MejorRasgo),
+    format('¿El personaje tiene ~w? (s/n)~n', [MejorRasgo]),
+    read_line_to_string(user_input, Respuesta),
+    (Respuesta == "s" -> Resp = s; Resp = n),
+    filtrar_personajes(Personajes, MejorRasgo, Resp, NuevosPersonajes),
+    delete(Rasgos, MejorRasgo, NuevosRasgos),
+    hacer_pregunta(NuevosPersonajes, NuevosRasgos).
+
+% Seleccionar el mejor rasgo (el mas discriminante)
+seleccionar_mejor_rasgo(Personajes, Rasgos, MejorRasgo) :-
+    maplist(contar_rasgo(Personajes), Rasgos, Contajes),
+    min_member(rasco_conteo(_, MejorRasgo), Contajes).
+
+contar_rasgo(Personajes, Rasgo, rasco_conteo(Diferencia, Rasgo)) :-
+    include(tiene_rasgo(Rasgo), Personajes, ConRasgo),
+    exclude(tiene_rasgo(Rasgo), Personajes, SinRasgo),
+    length(ConRasgo, ConCount),
+    length(SinRasgo, SinCount),
+    Diferencia is abs(ConCount - SinCount).
+
+tiene_rasgo(Rasgo, personaje(_, caracteristicas(Caracteristicas))) :-
+    member(Rasgo, Caracteristicas).
+
+% Filtrar personajes por respuesta a un rasgo
+filtrar_personajes(Personajes, Rasgo, s, NuevosPersonajes) :-
+    include(tiene_rasgo(Rasgo), Personajes, NuevosPersonajes).
+filtrar_personajes(Personajes, Rasgo, n, NuevosPersonajes) :-
+    exclude(tiene_rasgo(Rasgo), Personajes, NuevosPersonajes).
